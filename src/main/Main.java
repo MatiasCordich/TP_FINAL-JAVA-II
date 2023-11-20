15bca3a15fd5840ab8866fa3e5f76f325375836a
MatiasCordich <m.scordich@gmail.com>
2023-11-20 13:45:19 -0300

    Menu principal creado

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,19 +3,20 @@ package main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import menues.MenuRegistroLogin;
+import menues.MenuPrincipal;
 import modelos.usuarios.Cliente;
 import modelos.usuarios.Empleado;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
+        
         Scanner sc = new Scanner(System.in);
         ArrayList<Empleado> listaEmpleados = new ArrayList<Empleado>();
         ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
 
-        MenuRegistroLogin mLoginRegistro = new MenuRegistroLogin(listaEmpleados, listaClientes, sc);
+       MenuPrincipal menu = new MenuPrincipal(sc, listaEmpleados, listaClientes);
 
-        mLoginRegistro.iniciar();
+        menu.iniciar();
 
     }
 }
--- src/menues/MenuPrincipal.java
+++ src/menues/MenuPrincipal.java
@@ -1,5 +1,67 @@
 package menues;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import modelos.usuarios.Cliente;
+import modelos.usuarios.Empleado;
+
 public class MenuPrincipal {
 
+    private boolean continuar = true;
+    private Scanner sc;
+    ArrayList<Empleado> listaEmpleados;
+    ArrayList<Cliente> listaClientes;
+
+    public MenuPrincipal(Scanner sc, ArrayList<Empleado> listaEmpleados, ArrayList<Cliente> listaClientes) {
+        this.sc = sc;
+        this.listaEmpleados = listaEmpleados;
+        this.listaClientes = listaClientes;
+    }
+
+    private void mostrarOpciones() {
+        System.out.println("---------------------------------------------");
+        System.out.println("BIENVENIDO A PURIN MARKET");
+        System.out.println(" 0 - SALIR DEL PROGRAMA");
+        System.out.println(" 1 - REGISTRO/LOGIN");
+        System.out.println("---------------------------------------------");
+        System.out.print("Por favor, elija una de las siguientes opciones: ");
+    }
+
+    public void iniciar() {
+        while (continuar) {
+            int opcion = this.elegirOpcion();
+            this.realizarOpcion(opcion);
+        }
+    }
+
+    private int elegirOpcion() {
+        this.mostrarOpciones();
+        int opcion = this.sc.nextInt();
+        return opcion;
+    }
+
+    private void finalizar() {
+        this.continuar = false;
+        System.out.println("----------------------------------------------");
+        System.out.println("HAS SALIDO DEL PROGRAMA, QUE TENGAS UN BUEN DIA");
+        System.out.println("----------------------------------------------");
+    }
+
+    private void realizarOpcion(int opcion) {
+        switch (opcion) {
+            case 0:
+                this.finalizar();
+                break;
+            case 1:
+                MenuRegistroLogin mRegistroLogin = new MenuRegistroLogin(listaEmpleados, listaClientes, sc);
+                mRegistroLogin.iniciar();
+                break;
+            default:
+                System.out.println("----------------------------------------------");
+                System.out.println("OPCION INCORRECTA, INGRESE UNA OPCION VALIDA");
+                System.out.println("----------------------------------------------");
+                break;
+        }
+    }
 }
