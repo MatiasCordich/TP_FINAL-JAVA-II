7213ff47e904a77a7e5f0df32a3b671201899987
MatiasCordich <m.scordich@gmail.com>
2023-11-24 23:38:47 -0300

    Agregada las clases y funcionalidades creadas por Alan

---
+++ src/modelos/articulos/Subsidiado.java
@@ -0,0 +1,36 @@
+package modelos.articulos;
+
+public class Subsidiado extends Articulo {
+
+    private double precio_final;
+
+    // Constructor que herada los atributos de la clase abstracta
+    public Subsidiado(int id_articulo, String nombre, double precio_neto, int stock, char rubro) {
+        super(id_articulo, nombre, precio_neto, stock, rubro);
+        this.precio_final = 0;
+    }
+
+    // Setters
+    public void setPrecioFinal(double precio_final) {
+        this.precio_final = precio_final;
+    }
+
+    private void setDescuento() {
+
+        if (this.getRubro() == 'A') {
+            double descuento = this.getPrecio_neto() * (30 / 100);
+            double precio_final = this.getPrecio_neto() - descuento;
+            this.setPrecioFinal(precio_final);
+        } else if (this.getRubro() == 'B') {
+            double descuento = this.getPrecio_neto() * (24 / 100);
+            double precio_final = this.getPrecio_neto() - descuento;
+            this.setPrecioFinal(precio_final);
+           
+        } else {
+            double descuento = this.getPrecio_neto() * (15 / 100);
+            double precio_final = this.getPrecio_neto() - descuento;
+            this.setPrecioFinal(precio_final);
+
+        }
+    }
+}
